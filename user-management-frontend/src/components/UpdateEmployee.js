c22ffa8992d70b9fbc60dd87f6dbc5c485398ff8
Shahein Ockersz <shaheinockersz1234@gmail.com>
2023-07-22 13:43:51 +0530

    last commit by ockersz

--- user-management-frontend/src/components/UpdateEmployee.js
+++ user-management-frontend/src/components/UpdateEmployee.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Button, Paper } from "@mui/material";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import DialogBox from "./DialogBox";
 
